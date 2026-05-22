229e0103a6380d6733446a14f322d7f3248b4ecc
EkDinKaGamer <aaravpadole506@gmail.com>
2026-05-22 15:35:19 +0530

    Update calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,9 +1,7 @@
 "use client"
-
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
-
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 
@@ -54,17 +52,17 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, className, ...props }) => {
+          if (orientation === "left") {
+            return <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+          }
+          return <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+        },
       }}
       {...props}
     />
   )
 }
-Calendar.displayName = "Calendar"
 
+Calendar.displayName = "Calendar"
 export { Calendar }
