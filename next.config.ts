eeb1df0634d09c087dbcc12322337c4aa25b58cc
EkDinKaGamer <aaravpadole506@gmail.com>
2026-05-22 15:43:43 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export", // ✅ THIS replaces next export
 };
 
 export default nextConfig;
